f376f5f1c5c5aab09dc212ab66707e1fb7d027c3
Balcou Thomas <thomas.balcou@etudiant.univ-rennes1.fr>
2021-04-22 01:32:02 +0200

    fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tp7';
+export class AppComponent implements OnInit {
+  tableaux;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {    
+    this.getTableaux(); 
+  }
+  getTableaux(){
+    this.http
+      .get<any[]>('/api/tableau/all')
+      .subscribe(
+        (response) => {
+          this.tableaux = JSON.stringify(response);
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+        }
+      );
+   
+  }
+  onSubmit(form: NgForm) {
+    this.http
+    .post('/api/tableau/nouveau', form.value)
+    .subscribe(
+      () => {
+        console.log('Enregistrement terminé !');
+      },
+      (error) => {
+        console.log('Erreur ! : ' + error);
+      }
+    );
+}
+
+  
+  
 }
